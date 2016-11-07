859bf60deb5eec49c2ed6b8d279b31abf96c5684
anna <annateresavillanueva@gmail.com>
2016-11-07 21:37:38 +1100

    modified CbtLogSerializer to be a model serializer

--- cbt_logger/serializers.py
+++ cbt_logger/serializers.py
@@ -14,17 +14,17 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ('url', 'name')
 
-class CbtLogSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    published = serializers.BooleanField(required=False)
-    log_type = serializers.ChoiceField(choices=LOG_TYPE_CHOICES, default='quick_add')
+class CbtLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CbtLog
+        fields = ('id', 'title', 'published', 'log_type')
+        
 
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
         """
-        return Snippet.objects.create(**validated_data)
+        return CbtLog.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
